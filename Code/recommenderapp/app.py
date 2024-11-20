8f40e36d787c770874e2d018eb5956a21077ecd2
habib-itopa <mohammedhabibitopa@gmail.com>
2024-11-19 23:09:09 -0500

    fix table creation bug

--- Code/recommenderapp/app.py
+++ Code/recommenderapp/app.py
@@ -61,7 +61,7 @@ app.oauthclient = oauthclient
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 db = SQLAlchemy(app)
-
+db.app = app
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
@@ -645,7 +645,7 @@ def success():
     return render_template("success.html")
 
 
+with app.app_context():
+    db.create_all()
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(port=5000, debug=True)
